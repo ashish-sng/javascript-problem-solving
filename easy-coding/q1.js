778aa88fc3ba9d7b6003326431b8bd9b1abf8268
Ashish Singh <ashis@MacBook-Air.local>
2025-11-12 17:08:54 +0530

    feat: Implement a function to check if two strings are anagrams

---
+++ easy-coding/q1.js
@@ -0,0 +1,44 @@
+function anagramCheck(str1, str2) {
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  const charCount = {};
+
+  for (let e of str1) {
+    if (charCount.hasOwnProperty(e)) {
+      charCount[e]++;
+    } else {
+      charCount[e] = 1;
+    }
+  }
+
+  for (let e of str2) {
+    if (charCount.hasOwnProperty(e) && charCount[e] > 0) {
+      charCount[e]--;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function anagramCheck2(str1, str2) {
+  if (str1.length !== str2.length) return false;
+
+  const charCount = new Array(26).fill(0);
+
+  for (let i = 0; i < str1.length; i++) {
+    charCount[str1.charCodeAt(i) - 97]++;
+    charCount[str2.charCodeAt(i) - 97]--;
+  }
+
+  return charCount.every((count) => count === 0);
+}
+
+console.log(anagramCheck("listen", "silent"));
+console.log(anagramCheck("hello", "world"));
+
+console.log(anagramCheck2("listen", "silent"));
+console.log(anagramCheck2("hello", "world"));
